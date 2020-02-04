a032fd3307ffdf65e59e7cb4c2d8ce99d3b35076
Carles <carles.ruis@gmail.com>
2020-02-04 13:03:07 +0100

    remove commented code

--- app/src/main/java/com/carles/todo/mvvm/data/TodoRepository.kt
+++ app/src/main/java/com/carles/todo/mvvm/data/TodoRepository.kt
@@ -8,15 +8,14 @@ import com.carles.todo.mvvm.model.Todo
 
 class TodoRepository(private val dao: TodoDao, private val executors: AppExecutors) {
 
-
     fun getTodos(): LiveData<Resource<List<Todo>>> {
         val result = MediatorLiveData<Resource<List<Todo>>>()
         result.value = Resource.Loading()
 
         val dbSource = dao.getTodos()
-        result.addSource(dbSource) {
+        result.addSource(dbSource) { todos ->
             result.removeSource(dbSource)
-            result.value = Resource.Success(it)
+            result.value = Resource.Success(todos)
         }
 
         return result
